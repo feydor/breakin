4a64e6c5c2b8a09396df8245fd3a5e1bafdce77d
feydor <vreyes85739@outlook.com>
2022-06-21 16:03:07 -0500

    refactor out collision and rect functions

--- breakin.py
+++ breakin.py
@@ -12,23 +12,29 @@ BAR_HEIGHT = 50
 PROJ_BG = 0, 0xFF, 00
 PROJ_HEIGHT = BAR_HEIGHT
 PROJ_WIDTH = PROJ_HEIGHT
+PROJ_SPEED = 120
 BOUNCE_CONSTANT = 2
 COLLISION_PADDING = 10
-BAR_SPEED = 2
+BAR_SPEED = 120
 TARGET_BG = 0, 0, 0xFF
 TARGET_WIDTH = 100
 TARGET_HEIGHT = 50
 TARGET_PADDING = 25
+FPS = 60
+DELTA_TIME_SEC = 1.0/FPS
+BAR_Y = WINDOW_HEIGHT - 3*BAR_HEIGHT
 
 # x,y = left,top
 bar_x = WINDOW_WIDTH/2 - BAR_WIDTH/2
-bar_y = WINDOW_HEIGHT - 3*BAR_HEIGHT
-bar_dx = 2
-bar_dy = 1
+bar_dx = 0
+bar_dy = 0
 proj_x = WINDOW_WIDTH/2 - PROJ_HEIGHT/2
 proj_y = WINDOW_HEIGHT - 3*BAR_HEIGHT - PROJ_HEIGHT
-proj_dx = 1
-proj_dy = 1
+proj_dx = 0
+proj_dy = -1
+paused = True
+started = False
+window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 
 @dataclass
 class Target:
@@ -43,8 +49,60 @@ targets = [
     Target(x=100 + 4*(TARGET_WIDTH + TARGET_PADDING), y=100),
 ]
 
-paused = True
-window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+def bar_rect(x):
+    global BAR_Y
+    return pygame.Rect(x, BAR_Y, BAR_WIDTH, BAR_HEIGHT)
+
+def proj_rect(x, y):
+    return pygame.Rect(x, y, PROJ_WIDTH, PROJ_HEIGHT)
+
+def overlap(r1, r2):
+    return r1.colliderect(r2)
+
+def bar_collision(dt):
+    global bar_x
+    bar_nx = clamp(bar_x + bar_dx*BAR_SPEED*dt, 0, WINDOW_WIDTH - BAR_WIDTH)
+    if (overlap(bar_rect(bar_nx), proj_rect(proj_x, proj_y))):
+        return
+    bar_x = bar_nx
+
+def horiz_collision(dt):
+    global bar_x
+    global proj_x
+    global proj_dx
+    proj_nx = proj_x + proj_dx*PROJ_SPEED*dt
+    if proj_nx < 0 or proj_nx + PROJ_WIDTH > WINDOW_WIDTH or overlap(proj_rect(proj_nx, proj_y), bar_rect(bar_x)):
+        proj_dx *= -1
+        return
+    
+    proj_x = proj_nx
+    return None
+
+def vert_collision(dt):
+    global bar_x
+    global proj_y
+    global proj_dy
+    proj_ny = proj_y + proj_dy*PROJ_SPEED*dt
+    if proj_ny < 0 or proj_ny + PROJ_HEIGHT > WINDOW_HEIGHT or overlap(proj_rect(proj_x, proj_ny), bar_rect(bar_x)):
+        proj_dy *= -1
+        return
+    proj_y = proj_ny
+    return None
+
+def update(dt):
+    if not paused:
+        bar_collision(dt)
+        horiz_collision(dt)
+        vert_collision(dt)
+
+def render(window):
+    window.fill(COLOR_BG)
+    pygame.draw.rect(window, BAR_BG, bar_rect(bar_x))
+    pygame.draw.rect(window, PROJ_BG, proj_rect(proj_x, proj_y))
+    for target in targets:
+        if not target.dead:
+            pygame.draw.rect(window, TARGET_BG, (target.x, target.y, TARGET_WIDTH, TARGET_HEIGHT))
+    pygame.display.flip()
 
 def clamp(val, lower_bound, upper_bound):
     return max(lower_bound, min(val, upper_bound))
@@ -52,71 +110,26 @@ def clamp(val, lower_bound, upper_bound):
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_d:
-                paused = False
-                bar_dx = BAR_SPEED
-            if event.key == pygame.K_a:
-                paused = False
-                bar_dx = -BAR_SPEED
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_d or event.key == pygame.K_a:
-                bar_dx = 0
     
+    bar_dx = 0
     if pygame.key.get_pressed()[pygame.K_SPACE]:
         paused = True
-    if pygame.key.get_pressed()[pygame.K_a] and pygame.key.get_pressed()[pygame.K_d]:
-        bar_dx = 0
+    if pygame.key.get_pressed()[pygame.K_d]:
+        bar_dx += 1
+        if not started:
+            started = True
+            proj_dx = 1
+        if paused:
+            paused = False
+    if pygame.key.get_pressed()[pygame.K_a]:
+        bar_dx -= 1
+        if not started:
+            started = True
+            proj_dx = -1
+        if paused:
+            paused = False
 
-    if not paused:
-        nproj_x = proj_x + proj_dx
-        nproj_y = proj_y + proj_dy
+    
+    update(DELTA_TIME_SEC)
 
-        # collison detection of projectile
-        if nproj_x < 0 or nproj_x > WINDOW_WIDTH - PROJ_WIDTH:
-            nproj_x = proj_x - proj_dx
-            proj_dx *= -1
-        if proj_y < 0 or proj_y > WINDOW_HEIGHT - PROJ_HEIGHT:
-            nproj_y = proj_y - proj_dy
-            proj_dy *= -1
-
-        # check proj collision with bar
-        bar_rect = pygame.Rect(bar_x, bar_y, BAR_WIDTH, BAR_HEIGHT);
-        nproj_rect = pygame.Rect(nproj_x, nproj_y, PROJ_WIDTH, PROJ_HEIGHT);
-        if nproj_rect.colliderect(bar_rect):
-            # from right
-            if nproj_x < bar_x + BAR_WIDTH and nproj_x + PROJ_WIDTH > bar_x + BAR_WIDTH:
-                nproj_x += COLLISION_PADDING
-                proj_dx = BOUNCE_CONSTANT
-            #from left
-            elif nproj_x < bar_x + BAR_WIDTH and nproj_x + PROJ_WIDTH < bar_x + BAR_WIDTH:
-                nproj_x -= COLLISION_PADDING
-                proj_dx = -BOUNCE_CONSTANT
-            
-            # influence projectile refraction with bar
-            if bar_dx > 0:
-                proj_dx = BOUNCE_CONSTANT
-            elif bar_dx < 0:
-                proj_dx = -BOUNCE_CONSTANT
-
-            proj_dy *= -1
-        
-        for target in targets:
-            if not target.dead:
-                target_rect = pygame.Rect(target.x, target.y, TARGET_WIDTH, TARGET_HEIGHT)
-                if nproj_rect.colliderect(target_rect):
-                    proj_dy *= -1
-                    target.dead = True
-                    break
-
-        proj_x = nproj_x
-        proj_y = nproj_y
-        bar_x = clamp(bar_x + bar_dx, 0, WINDOW_WIDTH - BAR_WIDTH)
-
-    window.fill(COLOR_BG)
-    pygame.draw.rect(window, BAR_BG, (bar_x, bar_y, BAR_WIDTH, BAR_HEIGHT))
-    pygame.draw.rect(window, PROJ_BG, (proj_x, proj_y, PROJ_WIDTH, PROJ_HEIGHT))
-    for target in targets:
-        if not target.dead:
-            pygame.draw.rect(window, TARGET_BG, (target.x, target.y, TARGET_WIDTH, TARGET_HEIGHT))
-    pygame.display.flip()
+    render(window)
